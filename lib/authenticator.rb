3f5374c3f6a6bc3b8436355ca8b923ec897b3b2c
Ali Shanaakh <ashanaakh@gmail.com>
2018-12-22 17:50:19 +0200

    Add missing libs & fix ruby files indentation

--- lib/authenticator.rb
+++ lib/authenticator.rb
@@ -1,28 +1,43 @@
+# frozen_string_literal: true
+
+# Authenticator module.
 module Authenticator
-    class Auth
-        attr_reader :token
+  # Class that makes request to auth component (JWT).
+  class Auth
+    attr_reader :token
 
-        def initialize(username, password)
-            @username = username
-            @password = password
-            @token = authenticate!
-        end
-
-        def authenticate!
-            response = Net::HTTP.post(url, params)
-            JSON.parse(response.body, symbolize_names: true).dig(:token)
-        end
-
-        private
-
-        def params
-            { username: @username, password: @password }.to_json
-        end
-
-        def url
-            ENV.fetch('AUTH_URL', 'https://auth.palestinenights.com')
-            .yield_self { |url| "#{url}/auth" }
-            .yield_self { |it| URI(it) }
-        end
+    # @param username
+    # @param password
+    def initialize(username, password)
+      @username = username
+      @password = password
+      @token = authenticate!
     end
+
+    # Receives JWT token for with @username and @password.
+    # @return token
+    def authenticate!
+      response = Net::HTTP.post(url, json_params)
+      JSON.parse(response.body, symbolize_names: true).dig(:token)
+    end
+
+    private
+
+    def json_params
+      params.to_json
+    end
+
+    def params
+      {
+          username: @username,
+          password: @password
+      }
+    end
+
+    def url
+      ENV.fetch('AUTH_URL', 'https://auth.palestinenights.com')
+          .yield_self {|url| "#{url}/auth"}
+          .yield_self {|it| URI(it)}
+    end
+  end
 end
--- lib/feeder.rb
+++ lib/feeder.rb
@@ -1,45 +1,47 @@
 # frozen_string_literal: true
 
-require 'pry'
+require 'net/http'
+require 'json'
 
 require_relative 'authenticator'
 
 class Feeder
-    ROOT_PATH = "./data"
+  ROOT_PATH = "./data"
 
-    def initialize(username, password)
-        @authenticator = Authenticator::Auth.new(username, password)
+  def initialize(username, password)
+    @authenticator = Authenticator::Auth.new(username, password)
+  end
+
+  def start
+    all_items.each do |item|
+      response = Net::HTTP.post(url, item.to_json, headers)
+      puts "Status: #{response.code}"
     end
+  end
 
-    def start
-        all_items.each do |item|
-            response = Net::HTTP.post(url, item.to_json, headers)
-            binding.pry
-            puts "Status: #{response.code}"
-        end
-    end
+  def all_items
+    menu_items.flatten
+  end
 
-    def all_items
-        menu_items.flatten
-    end
+  private
 
-    private
+  def menu_items
+    files.map {|file| JSON.parse(file.yield_self {|file| File.read(file)})}
+  end
 
-    def menu_items
-        files.map { |file| JSON.parse(file.yield_self { |file| File.read(file) }) }
-    end
+  def url
+    ENV.fetch('API_URL', 'https://api.palestinenights.com')
+        .yield_self {|url| "#{url}/menu"}
+        .yield_self {|it| URI(it)}
+  end
 
-    def url
-        ENV.fetch('API_URL', 'https://api.palestinenights.com')
-        .yield_self { |url| "#{url}/menu" }
-        .yield_self { |it| URI(it) }
-    end
+  def headers
+    {
+        Authorization: "Bearer #{@authenticator.token}"
+    }
+  end
 
-    def headers
-        { "Authorization" => "Bearer #{@authenticator.token}" }
-    end
-
-    def files
-        Dir.glob("#{ROOT_PATH}/**/*.json")
-    end
+  def files
+    Dir.glob("#{ROOT_PATH}/**/*.json")
+  end
 end
